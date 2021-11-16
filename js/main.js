e0ca635a2da700c2bfe044d013989fed2ab9994c
beamingdeamon <51133964+beamingdeamon@users.noreply.github.com>
2021-11-17 00:02:00 +0600

    my project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let
+    index1 = document.querySelector(".river_cleaning"),
+    index2 = document.querySelector(".tree_planting"),
+    index3 = document.querySelector(".garbage_sorting")
+;
+
+index1.addEventListener("click", function(){
+    document.location.href = "index1.html";
+})
+index2.addEventListener("click", function(){
+    document.location.href = "index2.html";
+})
+index3.addEventListener("click", function(){
+    document.location.href = "index3.html";
+})
